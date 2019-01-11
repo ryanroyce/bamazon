498cb48b767f9255d0706865754ccaee05b16dad
dissonance <ryanminor01u@yahoo.com>
2019-01-11 16:32:17 -0600

    made purchasing functional, quantity updates in mysql table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,12 +7,12 @@ const cTable = require('console.table');
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "",
-    password: "",
+    user: "ryanminor",
+    password: "limabean13",
     database: "bamazon"
 });
 // confirming the connection in the CLI
-connection.connect(function (err) {
+connection.connect((err) => {
     if (err) throw err;
     console.log("\nWELCOME TO BAMAZON!\n");
     // running the function that will be written below to initiate the Node program
@@ -23,17 +23,17 @@ function start() {
     // with the bamazon database put in mysql command to select from the products table
     // greatbay activity was really helpful with this part    
     connection.query(
-        'SELECT * FROM products', function (err, res) {
+        'SELECT * FROM products', (err, res) => {
             if (err) throw err;
-            // displays the table from mysql
+            // displays the products table from mysql
             console.table(res);
-            // Define choice array to add products
+            // establishes an empty array to push the product_names into
             var productArr = [];
-            //create choices for each item
+            // pushes all of the product_names from the products table in mysql into the productArr
             for (var i = 0; i < res.length; i++) {
                 productArr.push(res[i].product_name)
             }
-            //using the inquirer package, create the questions that will be used in the initial prompts
+            // using the inquirer package, create the questions that will be used in the initial prompts
             inquirer.prompt([
                 {
                     type: 'rawlist',
@@ -46,7 +46,7 @@ function start() {
                     name: 'buy',
                     message: 'How many units would you like to buy?',
                     // to ensure that only numbers are used as input
-                    validate: function (value) {
+                    validate: (value) => {
                         if (isNaN(value) === false) {
                             return true;
                         }
@@ -54,6 +54,58 @@ function start() {
                     }
                 }
             ])
+                .then((purchase) => {
+                    // establishes two new variables to store the stock_quantity and updated stock_quantity inside of
+                    let quantity = res[productArr.indexOf(purchase.item)].stock_quantity;
+                    let updatedQuantity = quantity - purchase.buy;
+                    // also stores the cost from price in the products database as well as the totalCost that is the price multiplied by the quantity
+                    // we will need these variables later once an item is purchased
+                    let cost = res[productArr.indexOf(purchase.item)].price
+                    let totalCost = cost * purchase.buy;
+
+                    // if else statement to update stock_quantity, or determine if there is an insufficient stock_quantity
+                    if (purchase.buy <= quantity) {
+                        // connect to products database in mysql and updates the table
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: updatedQuantity
+                                },
+                                {
+                                    product_name: purchase.item
+                                }
+                            ],
+                            function (err) {
+                                if (err) throw err;
+                                console.log('\nORDER PLACED!');
+                                console.log(`YOUR TOTAL IS: $${totalCost}\n`);
+                                shopping();
+                            });
+                    }
+                    else {
+                        console.log("\nINSUFFICIENT QUANTITY!\n");
+                        shopping();
+                    }
+                });
         }
     );
 };
+
+function shopping() {
+    inquirer.prompt([
+        {
+            type: 'rawlist',
+            message: 'CONTINUE SHOPPING?',
+            name: 'stillshopping',
+            choices: ['YES', 'NO']
+        }
+    ]).then((purchase) => {
+        if (purchase.stillshopping === 'yes') {
+            start();
+        } else {
+            console.log("\nGOODBYE FROM BAMAZON!\n");
+            connection.end();
+        }
+    })
+}
